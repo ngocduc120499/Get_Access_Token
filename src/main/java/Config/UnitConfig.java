70af4d61db1fcdd41e366c34f70f7a687e68cc9f
nnduc <nnduc@tma.com.vn>
2021-09-13 15:40:47 +0700

    Update

--- src/main/java/Config/UnitConfig.java
+++ src/main/java/Config/UnitConfig.java
@@ -1,7 +1,7 @@
 package Config;
 
 public class UnitConfig {
-    public static  String tenant_id = "9a6b3b8e-c077-4faa-9ef2-414c742225d0";
-    public static String client_id = "da8921e3-0ac0-417a-b0b0-72251736aa1d";
-    public static String client_secret = "2wfC4KpD-.Nau2OOB4Yu-9iW4UD6.nhX4u";
+    public static  String tenant_id = "";
+    public static String client_id = "";
+    public static String client_secret = "";
 }
